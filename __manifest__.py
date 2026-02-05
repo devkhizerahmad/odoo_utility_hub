d7b6fc5695a97cb646aa21a8dca53749a796a209
Khizer Ahmad <devkhizerahmad@gmail.com>
2026-02-05 13:27:10 +0500

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Utility Hub',
+    'version': '1.0',
+    'summary': 'Custom Utility Hub Module',
+    'description': 'My first custom Odoo custom module',
+    'author': 'WebDev',
+    'category': 'Custom',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
